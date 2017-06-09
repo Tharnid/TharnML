ec91c48296e776a1782ec051319bb347008788a9
Tharnid <jeffmsmithdev@gmail.com>
2017-06-08 22:53:46 -0400

    next ait

--- AutomateIt/scrapeDO.py
+++ AutomateIt/scrapeDO.py
@@ -4,6 +4,5 @@ import requests
 page = requests.get('https://github.com/pricing/')
 tree = html.fromstring(page.content)
 print("Page Object:", tree)
-plans = tree.xpath('//h3[@class="   pricing-card-name display-heading-3"]/text()')
 pricing = tree.xpath('//span[@class="default-currency"]/text()')
-print("Plans:", plans, "\nPricing:", pricing)
+print("nPricing:", pricing)
