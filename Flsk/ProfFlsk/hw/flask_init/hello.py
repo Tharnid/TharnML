508b02e0ac3b6347bdf2b34f498c286331eb4a7b
Tharnid <jeffmsmithdev@gmail.com>
2018-02-19 18:06:06 -0500

    on to session stuff

--- Flsk/ProfFlsk/hw/flask_init/hello.py
+++ Flsk/ProfFlsk/hw/flask_init/hello.py
@@ -7,9 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello_from_flask():
-    return 'Hello from a Flask World!!!'
+    return 'Hello from a Flask World!'
 
 if __name__ == '__main__':
     host = os.getenv('IP', '0.0.0.0')
     port = int(os.getenv('port', 5000))
+    app.debug = True
     app.run(host=host, port=port)
