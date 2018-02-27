1152eef1bb3e80c0eef5c0be6afb8b42c7080838
Tharnid <jeffmsmithdev@gmail.com>
2018-02-27 11:39:28 -0500

    simple template

--- Flsk/ProfFlsk/hw/flask_init/hello.py
+++ Flsk/ProfFlsk/hw/flask_init/hello.py
@@ -1,8 +1,13 @@
 import os
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
+
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
