cb5f678e79d17f1b1a7a05a48d65e649b117b028
Tharnid <jeffmsmithdev@gmail.com>
2018-02-28 14:59:57 -0500

    done with login

--- Flsk/ProfFlsk/hw/flask_init/hello.py
+++ Flsk/ProfFlsk/hw/flask_init/hello.py
@@ -4,19 +4,21 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/hello')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    error = None
     if request.method == 'POST':
-        return 'username is ' + request.values["username"]
+        if valid_login(request.form['username'], request.form['password']):
+            return "Welcome back, %s" % request.form['username']
+        else:
+            error = 'Incorrect username and/or password'
+    return render_template('login.html', error=error)
+
+def valid_login(username, password):
+    if username == password:
+        return True
     else:
-        return '<form method="post" action="/login"><input type="text" name="username" /> <p><button type="submit">Submit</button></form>'
-
-
+        return False
 
 if __name__ == '__main__':
     host = os.getenv('IP', '0.0.0.0')
