dc79a49a5cb74de6e07576ede6308b9c51791f52
Tharnid <jeffmsmithdev@gmail.com>
2018-03-09 21:05:58 -0500

    removed cookies and added secret secret key lol

--- Flsk/ProfFlsk/hw/flask_init/hello.py
+++ Flsk/ProfFlsk/hw/flask_init/hello.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, request, render_template, redirect, url_for, flash, make_response
+from flask import Flask, request, render_template, redirect, url_for, flash, session
 
 app = Flask(__name__)
 
@@ -11,9 +11,8 @@ def login():
         if valid_login(request.form['username'], request.form['password']):
             flash("Successfully Logged In!!!")
             # return redirect(url_for('welcome', username=request.form.get('username'))
-            response = make_response(redirect(url_for('welcome')))
-            response.set_cookie('username', request.form.get('username'))
-            return response
+            session['username'] = request.form.get('username')
+            return redirect(url_for('welcome'))
         else:
             error = 'Incorrect username and/or password'
     return render_template('login.html', error=error)
@@ -26,20 +25,19 @@ def valid_login(username, password):
 
 @app.route('/logout')
 def logout():
-    response = make_response(redirect(url_for('login')))
-    response.set_cookie('username', '', expires=0)
-    return response
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 @app.route('/')
 def welcome():
-    username = request.cookies.get('username')
-    if username:
-        return render_template('welcome.html', username=username)
+    #  username = request.cookies.get('username')
+    if 'username' in session:
+        return render_template('welcome.html', username=session['username'])
     else:
         return redirect(url_for('login'))
 if __name__ == '__main__':
     host = os.getenv('IP', '0.0.0.0')
     port = int(os.getenv('port', 5000))
     app.debug = True
-    app.secret_key = 'Ylix12Zaur'# encodes sessions
+    app.secret_key = '\xa7\x1f\xe3K\xfc\xa1\xb1\xa7&|f\xca\xdf\xe8\x9e9u\xb5\xc7\xcd"+\xd7='# encodes sessions
     app.run(host=host, port=port)
