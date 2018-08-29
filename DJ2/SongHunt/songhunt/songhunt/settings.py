b45680bc8aaa92f4d0b09781524678677c57fa18
Tharnid <jeffmsmithdev@gmail.com>
2018-08-28 23:33:55 -0400

    started songhunt app and added songs and accounts apps to it

--- DJ2/SongHunt/songhunt/songhunt/settings.py
+++ DJ2/SongHunt/songhunt/songhunt/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'songs.apps.SongsConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'songhunt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['songhunt/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'songhunt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'songhuntdb',
+        'USER': 'aragoth',
+        'PASSWORD': 'ylix12ylix',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
